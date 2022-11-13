c138f8727be8e1ebadd68b6ed5ca87ebda816427
Devon Resendes <devon@Devons-MacBook-Air-2.local>
2022-11-13 13:11:45 -0500

    Updates

---
+++ precache-manifest.083377854a9cf4e2fb949543516f4969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7431be535b26c6c1177c",
+    "url": "/Gui_mapbox/static/css/main.8481e468.chunk.css"
+  },
+  {
+    "revision": "7431be535b26c6c1177c",
+    "url": "/Gui_mapbox/static/js/main.e804b149.chunk.js"
+  },
+  {
+    "revision": "1b56e6c63fac47657578",
+    "url": "/Gui_mapbox/static/js/runtime~main.08c187a2.js"
+  },
+  {
+    "revision": "b38706ce3cfeb1dd45bc",
+    "url": "/Gui_mapbox/static/js/2.bf41676e.chunk.js"
+  },
+  {
+    "revision": "e59448894c3b347a299b",
+    "url": "/Gui_mapbox/static/js/3.9f848081.chunk.js"
+  },
+  {
+    "revision": "3aea4b0fde8562bf873681d033366d4c",
+    "url": "/Gui_mapbox/index.html"
+  }
+];
